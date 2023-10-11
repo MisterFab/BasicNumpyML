b07c6b39c028291366e32b12ae95a11a5251339e
Leonard Kilthau <leonard@kilthau.info>
2023-10-11 21:27:29 +0200

    printing MSE

--- example_neural_net.py
+++ example_neural_net.py
@@ -26,12 +26,14 @@ def train_test_split_manual(X, y, test_size=0.3, random_seed=123):
 def train_neural_net_mlp(X_train, y_train, num_epochs=100, learning_rate=0.01, batch_size=64):
     num_features = X_train.shape[1]
     num_classes = len(np.unique(y_train))
-    num_hidden = 100
+    num_hidden = 50
     
     model = NeuralNetMLP(num_features, num_hidden, num_classes)
     num_batches = len(X_train) // batch_size
     
     for epoch in range(num_epochs):
+        mse_accumulated = 0.0
+        
         for i in range(num_batches):
             start_idx = i * batch_size
             end_idx = start_idx + batch_size
@@ -40,6 +42,8 @@ def train_neural_net_mlp(X_train, y_train, num_epochs=100, learning_rate=0.01, b
             y_batch = y_train[start_idx:end_idx]
             
             a_h, a_out = model.forward(X_batch)
+            mse_accumulated += compute_mse(a_out, model._int_to_onehot(y_batch, num_classes))
+            
             d_loss__dw_out, d_loss__db_out, d_loss__d_w_h, d_loss__d_b_h = model.backward(X_batch, a_h, a_out, y_batch)
             
             model.weight_out -= learning_rate * d_loss__dw_out
@@ -47,10 +51,15 @@ def train_neural_net_mlp(X_train, y_train, num_epochs=100, learning_rate=0.01, b
             model.weight_h -= learning_rate * d_loss__d_w_h
             model.bias_h -= learning_rate * d_loss__d_b_h
         
+        avg_mse_for_epoch = mse_accumulated / num_batches
+        
         if epoch % 10 == 0:
-            print("Epoch:", epoch)
+            print(f"Epoch: {epoch}, MSE: {avg_mse_for_epoch:.5f}")
         
     return model
+
+def compute_mse(predictions, y_true):
+    return np.mean((predictions - y_true) ** 2)
 
 def evaluate_model(model, X_test, y_test):
     _, predictions = model.forward(X_test)
